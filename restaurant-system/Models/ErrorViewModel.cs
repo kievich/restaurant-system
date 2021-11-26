7129840b521cfcd69a5e9e1ebb1b4583b61835aa
kievich <60324013+kievich@users.noreply.github.com>
2021-11-26 16:45:05 +0300

    Initial commit

---
+++ restaurant-system/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace restaurant_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
