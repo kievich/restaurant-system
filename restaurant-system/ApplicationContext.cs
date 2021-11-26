8726e4ee2aea5116c79d9b5b91fc5c2cbabbf5fe
kievich <60324013+kievich@users.noreply.github.com>
2021-11-26 19:45:47 +0300

    EntityFrameworkCore

---
+++ restaurant-system/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using restaurant_system.Models;
+
+namespace restaurant_system
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ restaurant-system/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restaurant_system.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+    }
+}
